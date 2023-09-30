d26a43690d8410a68ad68c675c6298f41c26b6a6
Neander Fontes <neanderffontes@gmail.com>
2023-09-30 17:30:06 +0100

    Update OperadoresAritmeticos.py
    Refatorando código

--- Estudonauta/OperadoresAritmeticos.py
+++ Estudonauta/OperadoresAritmeticos.py
@@ -10,43 +10,31 @@ print(f'Resultado de {num1} + {num2} = {numSoma}\n')
 
 print('--- Subtração ---')
 #Subtração
-num1 = 5
-num2 = 2
 numSubtracao = num1-num2
 print(f'Resultado de {num1} - {num2} = {numSubtracao}\n')
 
 print('*** Multiplicação ***')
 #Multiplicação
-num1 = 5
-num2 = 2
 numMultiplicacao = num1*num2
 print(f'Resultado de {num1} * {num2} = {numMultiplicacao}\n')
 
 print('/// Divisão Normal ///')
 #Divisão
-num1 = 5
-num2 = 2
 numDivisao = num1/num2
 print(f'Resultado de {num1} / {num2} = {numDivisao}\n')
 
 print('** Potência**')
 #Potência
-num1 = 5
-num2 = 2
 numPotencia = num1**num2
 print(f'Resultado de {num1} elevado à {num2} = {numPotencia}\n')
 
 print('/// Divisão por Inteiro ///')
 #Divisão Inteiro
-num1 = 5
-num2 = 2
 numDivisaoInteiro = num1//num2
 print(f'Resultado de {num1} / {num2} = {numDivisaoInteiro}\n')
 
 print('///Resto da Divisão ///')
 #Resto da Divisão
-num1 = 5
-num2 = 2
 numRestoDivisao = num1%num2
 print(f'Resultado do resto da divisão {num1} % {num2} = {numRestoDivisao}')
 
