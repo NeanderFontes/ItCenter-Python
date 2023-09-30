a8489749d4b4d30ba01d789564a98e858557766f
Neander Fontes <neanderffontes@gmail.com>
2023-09-30 16:35:03 +0100

    Update Ex_003.py

--- Estudonauta/Ex_003.py
+++ Estudonauta/Ex_003.py
@@ -4,5 +4,5 @@ print('===================== DESAFIO 002 =====================')
 print('Insira numeros a seguir:')
 num1, num2 = int(input('Insira primeiro numero: ')), int(input('Insira o segundo numero: '))
 soma = num1+num2
-print('Soma dos valores =', soma)
+print(f'Soma dos valores = {soma}')
 print('==================== Fim DO DESAFIO ====================')
