21f6d41b678d2f9c11007f734dc18730076f852a
Neander Fontes <neanderffontes@gmail.com>
2023-09-30 15:15:43 +0100

    Create Ex_002.py
    Exercicio de interação entre usuário e programador

---
+++ Estudonauta/Ex_002.py
@@ -0,0 +1,9 @@
+#Desafio 002: Interação entre Usuário - Programador
+
+print('===================== DESAFIO 002 =====================')
+print('Digite abaixo as datas de seu aniversário:')
+dia = input('Dia = ')
+mes = input('Mes = ')
+ano = input('Ano = ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
+print('==================== Fim DO DESAFIO ====================')
