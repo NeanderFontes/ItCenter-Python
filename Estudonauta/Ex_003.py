ffa7b9c0cbc9e9845875280ec751b2bd335714dc
Neander Fontes <neanderffontes@gmail.com>
2023-09-30 15:19:46 +0100

    Create Ex_003.py
    #Desafio 003: Interação entre Usuário Soma de valores - Programador

---
+++ Estudonauta/Ex_003.py
@@ -0,0 +1,8 @@
+#Desafio 003: Interação entre Usuário Soma de valores - Programador
+
+print('===================== DESAFIO 002 =====================')
+print('Insira numeros a seguir:')
+num1, num2 = int(input('Insira primeiro numero: ')), int(input('Insira o segundo numero: '))
+soma = num1+num2
+print('Soma dos valores =', soma)
+print('==================== Fim DO DESAFIO ====================')
