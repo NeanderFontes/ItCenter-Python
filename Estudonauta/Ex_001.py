f477ed76323dc9f9b844fd97939d1b688ce83bbf
Neander Fontes <neanderffontes@gmail.com>
2023-09-30 15:13:38 +0100

    Create Ex_001.py
    Exercicio de interação entre usuário e programador

---
+++ Estudonauta/Ex_001.py
@@ -0,0 +1,7 @@
+#Desafio 001: Interação entre Usuário - Programador
+
+print('===================== DESAFIO 001 =====================')
+nome = input('Qual o seu nome?')
+
+print('Seja Bem Vindo', nome, 'esse é o meu primeiro programa em Python')
+print('==================== Fim DO DESAFIO ====================')
