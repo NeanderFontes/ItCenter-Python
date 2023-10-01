da6524d13884773116efeba2db2e29cd5f9ef389
Neander Fontes <neanderffontes@gmail.com>
2023-10-01 01:18:23 +0100

    Create Ex_007.py
    #Desafio 007: Interação entre Usuário - Programador
    #Desenvolver um programa que leia as duas notas de um aluno e calcular a média

---
+++ Estudonauta/Ex_007.py
@@ -0,0 +1,7 @@
+#Desafio 007: Interação entre Usuário - Programador
+#Desenvolver um programa que leia as duas notas de um aluno e calcular a média
+
+print('===================== DESAFIO 002 =====================')
+num1, num2 = int(input('Insira primeira nota: ')), int(input('Insira segunda nota: '))
+print(f'1ª Nota {num1} e 2ª Nota {num2}\nMédia = {(num1+num2)/2}')
+print('==================== Fim DO DESAFIO ====================')
