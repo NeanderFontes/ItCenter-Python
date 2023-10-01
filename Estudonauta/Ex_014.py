3989a1bc097bd5a83638bfe633c59905d4a16aa1
Neander Fontes <neanderffontes@gmail.com>
2023-10-01 10:46:37 +0100

    Create Ex_014.py
    #Desafio 014: Interação entre Usuário - Programador
    #Fazer um programa que simule a conversão de temperaturas de ºC para ºF

---
+++ Estudonauta/Ex_014.py
@@ -0,0 +1,8 @@
+#Desafio 014: Interação entre Usuário - Programador
+#Fazer um programa que simule a conversão de temperaturas de ºC para ºF
+
+print('='*22, 'DESAFIO 00', '='*22)
+print('Conversor de ºC para ºF')
+valorTemperatura = float(input('Insira o valor da temperatura em ºC: '))
+print(f'{valorTemperatura}ºC = {valorTemperatura*1.8}')
+print('='*22, 'FIM DO DESAFIO', '='*22)
