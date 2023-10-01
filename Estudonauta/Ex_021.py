2681e9fcda9fc2900c44d4cebe63d883d85c259e
Neander Fontes <neanderffontes@gmail.com>
2023-10-01 15:59:35 +0100

    Create Ex_021.py
    Exercicio não funcional

---
+++ Estudonauta/Ex_021.py
@@ -0,0 +1,13 @@
+#Desafio 021: Interação entre Usuário - Programador
+
+#Inicio do programa
+print('='*22, 'DESAFIO 021', '='*22)
+print()
+
+#Declaração de variáveis e entrada de dados:
+
+
+#Saída de dados formatados:
+
+#Finalização do programa:
+print('='*22, 'FIM DO DESAFIO', '='*22)
