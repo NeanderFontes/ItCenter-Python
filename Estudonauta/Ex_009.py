53dd5be07ed8913fc7e55e427e2c77cacfae33d7
Neander Fontes <neanderffontes@gmail.com>
2023-10-01 01:18:53 +0100

    Create Ex_009.py
    #Desafio 009: Interação entre Usuário - Programador
    #Faça um programa que leia um numero inteiro qualquer e mostre na tela sua tabuada

---
+++ Estudonauta/Ex_009.py
@@ -0,0 +1,19 @@
+#Desafio 009: Interação entre Usuário - Programador
+#Faça um programa que leia um numero inteiro qualquer e mostre na tela sua tabuada
+
+print('='*22, 'DESAFIO 002', '='*22)
+print('Tabuada')
+numEntrada = int(input('Insira um valor inteiro: '))
+print('Tabuada de {}'.format(numEntrada))
+print(f'Tabuada de 0 x {numEntrada} = {numEntrada*0}')
+print(f'Tabuada de 1 x {numEntrada} = {numEntrada*1}')
+print(f'Tabuada de 2 x {numEntrada} = {numEntrada*2}')
+print(f'Tabuada de 3 x {numEntrada} = {numEntrada*3}')
+print(f'Tabuada de 4 x {numEntrada} = {numEntrada*4}')
+print(f'Tabuada de 5 x {numEntrada} = {numEntrada*5}')
+print(f'Tabuada de 6 x {numEntrada} = {numEntrada*6}')
+print(f'Tabuada de 7 x {numEntrada} = {numEntrada*7}')
+print(f'Tabuada de 8 x {numEntrada} = {numEntrada*8}')
+print(f'Tabuada de 9 x {numEntrada} = {numEntrada*9}')
+print(f'Tabuada de 10 x {numEntrada} = {numEntrada*10}')
+print('='*22, 'FIM DO DESAFIO', '='*22)
