7cf115aed2b4d70980f67ebcfdab7bde237e6ca6
Neander Fontes <neanderffontes@gmail.com>
2023-10-01 01:17:52 +0100

    Create Ex_006.py
    #Desafio 006: Interação entre Usuário - Programador
    #Criar um programa que leia um numero e mostre o seu dobro, triplo e raiz quadrada.

---
+++ Estudonauta/Ex_006.py
@@ -0,0 +1,7 @@
+#Desafio 006: Interação entre Usuário - Programador
+#Criar um programa que leia um numero e mostre o seu dobro, triplo e raiz quadrada.
+
+print('===================== DESAFIO 002 =====================')
+numEntrada = int(input('Insira um valor numerico: '))
+print(f'Numero digitado = {numEntrada}\nDobro = {numEntrada*2}\nTriplo = {numEntrada*3}\nRaiz Quadrada = {numEntrada**2}')
+print('==================== Fim DO DESAFIO ====================')
