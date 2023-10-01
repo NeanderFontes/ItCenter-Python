03e1f6cac247c04adbe8ee6c106acd1a237a1382
Neander Fontes <neanderffontes@gmail.com>
2023-10-01 01:17:39 +0100

    Create Ex_005.py
    #Desafio 005: Interação entre Usuário - Programador
    #Numero Antecessor e Sucessor

---
+++ Estudonauta/Ex_005.py
@@ -0,0 +1,8 @@
+
+#Desafio 005: Interação entre Usuário - Programador
+#Numero Antecessor e Sucessor
+
+print('===================== DESAFIO 002 =====================')
+numEntrada = int(input('Digite um valor inteiro: '))
+print(f'Numero Digitado = {numEntrada}\nSucessor = {numEntrada+1}\nAntecessor = {numEntrada-1}')
+print('==================== Fim DO DESAFIO ====================')
