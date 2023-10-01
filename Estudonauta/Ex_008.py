d8e74b091f18010269bc01d2548077ffeb79d77b
Neander Fontes <neanderffontes@gmail.com>
2023-10-01 01:18:35 +0100

    Create Ex_008.py
    #Desafio 008: Interação entre Usuário - Programador
    #Escreva um programa que leia um valor em metros e exiba os valores convertido em centimetros e milimetros.

---
+++ Estudonauta/Ex_008.py
@@ -0,0 +1,9 @@
+#Desafio 008: Interação entre Usuário - Programador
+#Escreva um programa que leia um valor em metros e exiba os valores convertido em centimetros e milimetros.
+
+print('===================== DESAFIO 002 =====================')
+print('Conversão de Metro em Centimetros e Milimetros:')
+numEntrada = int(input('Insira o valor em Metros: '))
+print(f'Valor de {numEntrada} metros equivale a')
+print(f'{numEntrada}m\n{numEntrada*100}cm\n{numEntrada*1000}mm')
+print('==================== Fim DO DESAFIO ====================')
